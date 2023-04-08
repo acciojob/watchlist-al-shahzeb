49c0121769a773aa036dc07b4bdc0ff1420d5e93
al-shahzeb <alishahzeb453@gmail.com>
2023-04-09 02:29:47 +0530

    updated

--- src/main/java/com/driver/MovieController.java
+++ src/main/java/com/driver/MovieController.java
@@ -11,8 +11,8 @@ import java.util.List;
 @RequestMapping("/movies")
 public class MovieController {
 
-    @Autowired
-    MovieService movieService;
+    //@Autowired
+    MovieService movieService=new MovieService();
 
     @PostMapping("/add-movie")
     public ResponseEntity<String> addMovie(@RequestBody Movie movie){
